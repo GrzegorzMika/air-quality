3633a35c4e353c380126c70074eed2e67f2dd99d
Grzegorz <g.w.mika@gmail.com>
2020-07-09 16:11:43 +0200

    correct deploy.sh

--- dashboard/deploy.sh
+++ dashboard/deploy.sh
@@ -1,8 +1,8 @@
-docker build --rm -t dashboard_new -f Dockerfile .
-docker run \
+docker build --rm -t dashboard -f Dockerfile .
+docker run -d \
           --network=host \
           -p 3838:3838 \
           -e MYSQL_USER='dashboard' \
           -e MYSQL_PASSWORD='dashboard' \
           -e MYSQL_HOST='192.168.1.103' \
-          dashboard_new
+          dashboard
