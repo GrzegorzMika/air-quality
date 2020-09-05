eae111e8e4b7e85c0b87b343de8872c0df62bfa0
Grzegorz <g.w.mika@gmail.com>
2020-09-05 21:36:45 +0200

    fix backend

--- dashboard/deploy.sh
+++ dashboard/deploy.sh
@@ -6,5 +6,5 @@ docker run -d \
           -p 3838:3838 \
           -e MYSQL_USER='dashboard' \
           -e MYSQL_PASSWORD='dashboard' \
-          -e MYSQL_HOST='192.168.1.103' \
+          -e MYSQL_HOST='192.168.1.100' \
           dashboard
