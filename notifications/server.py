6603e295479872bca982d3ebc62eb9999c6c4e3e
Grzegorz <g.w.mika@gmail.com>
2020-11-15 21:39:33 +0100

    add descriptions

--- notifications/server.py
+++ notifications/server.py
@@ -40,6 +40,11 @@ def connect_database(username, password, database, host, port="3306"):
 
 
 def parse_relaxation(relaxation):
+    """
+    Helper function to verify if time format is as expected and to split it.
+    :param relaxation: string representing the grace period
+    :return: tuple consisting of integers representing the hour, minute and second
+    """
     assert re.match(r"[0-9][0-9]:[0-9][0-9]:[0-9][0-9]", relaxation), 'Relaxation is expected in a form HH:MM:SS, ' \
                                                                       'but {} was provided'.format(relaxation)
 
@@ -48,6 +53,14 @@ def parse_relaxation(relaxation):
 
 
 def check_if_sent(cursor, database, sensor, relaxation):
+    """
+    Verify if the notification for given sensor was sent or not within the period specified by relaxation.
+    :param cursor: cursor to database connection
+    :param database: database connection
+    :param sensor: name of the sensor for which the notifications are being sent, must coincide with the name of a table
+    :param relaxation: string representing the grace period
+    :return: True if the notification was sent within the period specified by relaxation, False otherwise
+    """
     hour, minute, second = parse_relaxation(relaxation)
     time_shift = datetime.now() - timedelta(hours=hour, minutes=minute, seconds=second)
 
@@ -69,19 +82,35 @@ def check_if_sent(cursor, database, sensor, relaxation):
         return False
 
 
-def store_info(mydb, cursor, sensor):
+def store_info(cursor, database, sensor, info='Notification sent!'):
+    """
+    Store the info about sent notifications for a given sensor.
+    :param cursor: cursor to the database connection
+    :param database: database connection
+    :param sensor: name of the sensor for which the notification are being sent, must coincide with the table name
+    :param info: additional information to store with each recorded notification
+    """
     time = datetime.now()
-    info = 'Notification sent!'
-    query = "INSERT INTO notifications (timestamp, sensor, info) VALUES (\"{}\", \"{}\", \"{}\")".format(time, sensor, info)
+    query = "INSERT INTO notifications (timestamp, sensor, info) VALUES (\"{}\", \"{}\", \"{}\")".format(time, sensor,
+                                                                                                         info)
 
     try:
         cursor.execute(query)
-        mydb.commit()
+        database.commit()
     except Exception as e:
         logging.error(e)
 
 
 def observe(cursor, database, sensor, lower_threshold, upper_threshold):
+    """
+    Observe the level of a given sensor and verify if it is between thresholds.
+    :param cursor: cursor to database connection
+    :param database: database connection
+    :param sensor: name of the sensor for which the notification are being sent, must coincide with the table name
+    :param lower_threshold: upper bound for allowed values for a sensor value
+    :param upper_threshold: lower bound for allowed values for a sensor value
+    :return: tuple of logical indicator if any of thresholds is crossed and the measurement value
+    """
     query = "SELECT * " \
             "FROM {}.{} " \
             "ORDER BY timestamp DESC " \
@@ -92,7 +121,7 @@ def observe(cursor, database, sensor, lower_threshold, upper_threshold):
         results = cursor.fetchall()
     except Exception as e:
         logging.error(e)
-        results = [datetime.now, -1]
+        results = [(datetime.now, -1)]
 
     value = results[0][1]
 
