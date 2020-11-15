9bbc10c5896fd07e8c9107ec29368b799e26db60
Grzegorz <g.w.mika@gmail.com>
2020-11-15 21:58:12 +0100

    add credentials template

---
+++ credentials_template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export MYSQL_USER='' # Database user with write access
+export MYSQL_PASSWORD='' # Database user with write access
+export MYSQL_HOST='' # Host of the SQL user
+export AIR_QUALITY_USER='' # E-mail address to send notifications from
+export AIR_QUALITY_PASSWORD='' # E-mail address password
+export DB_USER='' # Database user with read access
+export DB_PASSWORD='' # Database user with read access
