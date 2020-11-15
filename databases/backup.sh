f1a7a7bcbd27f6f0b553510e69c295ac48877108
Grzegorz <g.w.mika@gmail.com>
2020-11-16 00:16:33 +0100

    simplify database backup

--- databases/backup.sh
+++ databases/backup.sh
@@ -4,4 +4,4 @@ YEAR=$(date +%Y)
 MONTH=$(date +%m)
 DAY=$(date +%d)
 mkdir -p air/$YEAR/$MONTH/$DAY
-mysqldump -u$DB_OWNER -p$DB_PSSWD air > air/$YEAR/$MONTH/$DAY/backup.sql
+mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD air > air/$YEAR/$MONTH/$DAY/backup.sql
